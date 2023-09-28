bccb3166a69a78ff8c15867a5f6fe91d929f483a
Birky-andrie <145018370+Birky-andrie@users.noreply.github.com>
2023-09-28 20:55:50 +0800

    Add files via upload

---
+++ PACURIBOT_MinSecConverter.py
@@ -0,0 +1,10 @@
+
+# Get input from the person
+seconds = int(input("Enter an integer in seconds: "))
+
+# Calculate minutes and remaining seconds
+minutes = seconds // 60
+remaining_seconds = seconds % 60
+
+# Display the result
+print(f"{seconds} seconds is {minutes} minutes and {remaining_seconds} seconds")
